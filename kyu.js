831e4243999400dd35931cc7227a020c55cab240
laisneves92 <jraya6904@gmail.com>
2025-05-16 08:39:10 -0300

    alteração na linha ramificada

--- kyu.js
+++ kyu.js
@@ -1,1 +1,5 @@
 console.log("Hello, World!");
+// This is a simple JavaScript program that prints "Hello, World!" to the console.
+// It serves as a basic example of how to write and run JavaScript code.
+// You can run this code in any JavaScript environment, such as a web browser's console or Node.js.
+// To run this code in a web browser, open the developer tools (usually F12 or right-click and select "Inspect"), go to the "Console" tab, and paste the code there.
